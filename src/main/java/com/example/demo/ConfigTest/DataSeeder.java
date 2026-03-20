30b30f833a2553d084da0e17485986cca56d735a
Richard-Alves167 <riichardd.08@gmail.com>
2026-03-20 08:21:09 -0300

    FEAT: implementa Seed Data para a entidade Credencial

--- src/main/java/com/example/demo/ConfigTest/DataSeeder.java
+++ src/main/java/com/example/demo/ConfigTest/DataSeeder.java
@@ -1,7 +1,10 @@
 package com.example.demo.ConfigTest;
 
+import com.example.demo.DTO.Credencial.CredencialCreateDTO;
 import com.example.demo.DTO.Usuario.UsuarioCreateDTO;
+import com.example.demo.Model.Credencial;
 import com.example.demo.Model.Usuario;
+import com.example.demo.Repository.CredencialRepository;
 import com.example.demo.Repository.UsuarioRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
@@ -37,6 +40,33 @@ public class DataSeeder {
                 usuarioDTO = new UsuarioCreateDTO("Lucas", "Pereira", "21123456789", LocalDate.of(2001, 5, 26));
                 usuario = modelMapper.map(usuarioDTO, Usuario.class);
                 usuarioRepository.save(usuario);
+            }
+        };
+    }
+    @Bean
+    @Order(2)
+    CommandLineRunner seedCredencial(CredencialRepository credencialRepository, ModelMapper modelMapper) {
+        return args -> {
+            if (credencialRepository.count() == 0) {
+                CredencialCreateDTO credencialDTO = new CredencialCreateDTO("richard.alves@gmail.com", "1234567890");
+                Credencial credencial = modelMapper.map(credencialDTO, Credencial.class);
+                credencialRepository.save(credencial);
+
+                credencialDTO = new CredencialCreateDTO("taylan.gonzaga@gmail.com", "1234567890");
+                credencial = modelMapper.map(credencialDTO, Credencial.class);
+                credencialRepository.save(credencial);
+
+                credencialDTO = new CredencialCreateDTO("juan.santos@gmail.com", "1234567890");
+                credencial = modelMapper.map(credencialDTO, Credencial.class);
+                credencialRepository.save(credencial);
+
+                credencialDTO = new CredencialCreateDTO("guilherme.pirozi@gmail.com", "1234567890");
+                credencial = modelMapper.map(credencialDTO, Credencial.class);
+                credencialRepository.save(credencial);
+
+                credencialDTO = new CredencialCreateDTO("lucas.pereira@gmail.com", "1234567890");
+                credencial = modelMapper.map(credencialDTO, Credencial.class);
+                credencialRepository.save(credencial);
             }
         };
     }
